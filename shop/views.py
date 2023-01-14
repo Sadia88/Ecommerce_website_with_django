5eb3549b67adc217678ad928f411926751e36a59
Sadia88 <sadiasharmin460280@gmail.com>
2023-01-14 20:48:10 +0600

    urls is added on shop

--- shop/views.py
+++ shop/views.py
@@ -3,3 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'shop/index.html')
+
+
+
+def about():
+    pass
+
+def contact():
+    pass
+
+
+def tracker():
+    pass
+
+def search():
+    pass
+
+def productview():
+    pass
+
+
+def checkout():
+    pass

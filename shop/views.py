a28138fd7f3a53844dcc650ac5df780af00d3546
Sadia88 <sadiasharmin460280@gmail.com>
2023-01-15 19:59:16 +0600

    basic tamplate  is added on shop

--- shop/views.py
+++ shop/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 def index(request):
+  
     return render(request,'shop/index.html')
 
 
 
 def about(request):
-    pass
+   return render(request,'shop/about.html')
 
 def contact(request):
     return HttpResponse("contact")

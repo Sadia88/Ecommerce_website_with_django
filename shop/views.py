a7334b9b3d9028981c2037ca0867a66d8861ef17
Sadia88 <sadiasharmin460280@gmail.com>
2023-01-16 19:42:58 +0600

    basic tamplate  is added on shop

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
-
+from math import ceil
 # Create your views here.
 def index(request):
-  
-    return render(request,'shop/index.html')
+    # products=Product.objects.all()
+   
+    # n=len(products)
+    # nSlides=n//4+ceil(n/4)-(n//4)
+    # # params={'nSlides':nSlides, "range":range(1,nSlides),'product':products}
+    # allProds=[[products, range(1, nSlides), nSlides],[products, range(1, nSlides), nSlides]]
+     products= Product.objects.all()
+     allProds=[]
+     catprods= Product.objects.values('category', 'id')
+    #  print(catprods)
+     cats= {item["category"] for item in catprods}
+     
+     for cat in cats:
+            prod=Product.objects.filter(category=cat)
+            # print(prod)
+            n = len(prod)
+            nSlides = n // 4 + ceil((n / 4) - (n // 4))
+            allProds.append([prod, range(1, nSlides), nSlides])
+            params={'allProds':allProds }
+            
+     return render(request,'shop/index.html',params)
 
 
 

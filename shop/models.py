7f23f54b5d985aa52bcf57cde7d08967db5e89e2
Sadia88 <sadiasharmin460280@gmail.com>
2023-01-17 19:20:07 +0600

    popover is addedd

--- shop/models.py
+++ shop/models.py
@@ -12,3 +12,12 @@ class Product(models.Model):
     pub_date=models.DateField()
     def __str__(self):
         return self.product_name
+class Contact(models.Model):
+    msg_id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=50)
+    email=models.EmailField(max_length=50,default='')
+    phone=models.CharField(max_length=50,default='')
+    desc=models.CharField(max_length=1000,default='')
+   
+    def __str__(self):
+        return self.email
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product,Contact
 from math import ceil
 # Create your views here.
 def index(request):
@@ -34,13 +34,15 @@ def about(request):
 def contact(request):
     if(request.method=='POST'):
         name=request.POST.get('name','')
-        email=request.POST.get('name','')
-        phone=request.POST.get('name','')
-        desc=request.POST.get('name','')
+        email=request.POST.get('email','')
+        phone=request.POST.get('phone','')
+        desc=request.POST.get('desc','')
+        contact=Contact(name=name,email=email,phone=phone,desc=desc)
+        contact.save()
     return render(request,'shop/contact.html')
 
 
-def tracker(request):
+def tracker(request):                                
      return render(request,'shop/tracker.html')
 
 def search(request):

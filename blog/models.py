9e74c4a1f935477592927d2087bfc27dfe202385
Sadia88 <sadiasharmin460280@gmail.com>
2023-01-19 11:01:09 +0600

    blogpost page is designed

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    post_id=models.AutoField(primary_key=True)
+    title=models.CharField(max_length=200)
+    head0=models.CharField(max_length=500)
+    head1=models.CharField(max_length=500)
+    head2=models.IntegerField(max_length=500)
+    thumbnail=models.ImageField(upload_to='blog/images',default='')
+   
+    pub_date=models.DateField()
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'blog/index.html')
+def blogPost(request):
+    return render(request,'blog/blogpost.html')
